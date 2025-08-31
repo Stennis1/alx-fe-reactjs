26105c18b215f4bc1a3e93f030e429143a72415a
Stennis1 <stennismith@gmail.com>
2025-08-31 23:55:19 +0000

    feat: add additional functions and pages to export to App.jx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,14 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './Header'
+import MainContent from './MainContent'
+import Footer from './footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* Custom Welcome Message*/}
+      {/* Custom Welcome Message */}
       <WelcomeMessage />
+
+      {/*  */}
+      <Header />
+
+      {/*  */}
+      <MainContent />
+      
+      {/*  */}
+      <Footer />
 
       <div>
         <a href="https://vite.dev" target="_blank">
