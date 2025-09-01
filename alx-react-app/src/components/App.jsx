0a5eb3d4fc866b234c9d9799405cd634d096f902
Stennis1 <stennismith@gmail.com>
2025-09-01 00:01:49 +0000

    fix: correct import path for MainContent component and add MainContent component

--- alx-react-app/src/components/App.jsx
+++ alx-react-app/src/components/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from './Header';
-import MainContent from '../MainContent';
+import MainContent from './MainContent';
 import Footer from './Footer';
 
 function App() {
