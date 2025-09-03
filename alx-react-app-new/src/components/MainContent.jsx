6312d26f4fcecbeeb85bf564ef9c4e6bbcec43ce
Stennis1 <stennismith@gmail.com>
2025-09-03 23:57:05 +0000

    fix: add inline styling to Main and Footer components and build Counter.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent() {
     return (
         <main>
-            <p>I love to visit New York, Paris, and Tokyo.</p>
+            <p style={{ fontSize: '12', backgroundColor: 'blue' }}>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
 }
--- alx-react-app-new/src/components/WelcomeMessage.jsx
+++ alx-react-app-new/src/components/WelcomeMessage.jsx
@@ -1,8 +1,8 @@
 function WelcomeMessage() {
     return (
         <div>
-            <h1>Hello everyone, I am learning React at ALX!</h1>
-            <p>I am learning about JSX!</p>
+            <h1 style={{ fontWeight: "bold"}}>Hello everyone, I am learning React at ALX!</h1>
+            <p style={{ fontFamily: 'cursive', color: 'white'}}>I am learning about JSX!</p>
         </div>
     );
 }
