6127573866fa8d63926c27c8f0bdf57adcb75632
Stennis1 <stennismith@gmail.com>
2025-09-07 20:48:47 +0000

    feat: add new react dir with changes to alx-react-app-props

---
+++ my-company/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Navbar from './Navbar'
+import Home from './Home'
+import About from './About'
+import Services from './Services'
+import Contact from './Contact'
+import Footer from './Footer'
+
+function App() {
+
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/about' element={<About/>}></Route>
+          <Route path='/services' element={<Services/>}></Route>
+          <Route path='/contact' element={<Contact/>}></Route>
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  )
+}
+
+export default App
